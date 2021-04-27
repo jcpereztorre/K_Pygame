e5ee66fe552f82ffe63213b47a2571d1a256d752
Jose Carlos Perez Torre <josecarlospereztorre@MacBook-Pro-de-Jose.local>
2021-04-27 22:55:04 +0200

    Bolas con rebote en pantalla

---
+++ hello.py
@@ -0,0 +1,23 @@
+import pygame as pg
+import sys
+
+pg.init()
+pantalla = pg.display.set_mode((600, 400))
+pg.display.set_caption("Hola")
+
+game_over = False
+
+while not game_over:
+    # Gestión de eventos
+    for evento in pg.event.get():
+        if evento.type == pg.QUIT:
+            game_over = True
+
+    # Gestión del estado
+    print('Hola mundo')
+
+    # Refrescar pantalla
+    pantalla.fill((0, 255,0))
+    pg.display.flip()
+
+fin_juego()
